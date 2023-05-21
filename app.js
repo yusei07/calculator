4e6388fcd2c3de115d26e1bcd55c2948df57af8b
nora <usernamesaresoharddtomake@gmail.com>
2023-05-21 17:55:57 +0800

    refactor codebase

--- app.js
+++ app.js
@@ -1,78 +1,130 @@
-const numberBtn = document.querySelectorAll('[data-number]')
-const operatorBtn = document.querySelectorAll('[data-operator]')
+const numberBtn = document.querySelectorAll('[data-number]');
+const operatorBtn = document.querySelectorAll('[data-operator]');
+const currentOperandTextEl= document.querySelector('.current-operand');
+const previousOperandTextEl = document.querySelector('.previous-operand');
+const clearBtn = document.querySelector('.clear-btn');
+const delBtn = document.querySelector('.del-btn');
+const equalBtn = document.querySelector('#equal-btn');
+// const floatBtn = document.querySelector('#float-btn')
 
-const currentOperation = document.querySelector('current-operand')
-const previousOperation = document.querySelector('previous-operand')
 
-const clearBtn = document.querySelector('clear-btn')
-const delBtn = document.querySelector('del-btn')
+// init
+class Calculator {
+  constructor(previousOperandTextEl, currentOperandTextEl) {
+    this.previousOperandTextEl = previousOperandTextEl
+    this.currentOperandTextEl = currentOperandTextEl
+    this.clear()
+  }
 
-const equalBtn = document.querySelector('equal-btn')
-const floatBtn = document.querySelector('float-btn')
+  clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
+  }
 
-// initialize
-let setCurrentScreen = ''
-let resetScreen = false
+  delete(number) {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1);
+  }
+
+  appendNumber(number) {
+    if (number === '.' && this.currentOperand.includes('.')) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString()
+  }
+
+  chooseOperation(operation) {
+    if (this.currentOperand === '') return
+    if (this.previousOperand !== '') {
+      this.operate() // computes automatically when another operation is performed
+    }
+
+    this.operation = operation
+    this.previousOperand = this.currentOperand
+    this.currentOperand = '' // set back to empty str
+  }
+
+  operate() {
+    const prev = parseFloat(this.previousOperand)
+    const current = parseFloat(this.currentOperand)
+    let result
+    
+    if (isNaN(prev) || isNaN(current)) return
+
+    switch (this.operation) {
+      case '+':
+        result = prev + current
+        break
+      case '-':
+        result = prev - current
+        break
+      case '*':
+      case '×':
+        result = prev * current
+        break
+      case '÷':
+      case '/':
+        if (current === 0) {
+          return null
+          break
+        }
+        else {
+          result = prev / current
+          break
+        }
+      default:
+        return
+    }
+    this.currentOperand = result
+    this.operation = undefined
+    this.previousOperand = ''
+  }
+
+  updateDisplay() {
+    this.currentOperandTextEl.innerText = this.currentOperand
+    this.previousOperandTextEl.textContent = this.previousOperand
+  }
+
+}
 
 
 // keyboard input
-window.addEventListener('keydown', handleKeyboardInput)
-
 function handleKeyboardInput(e) {
-  if (e.key >= 0 && e.key <= 9) appendNumber(e.key)
-}
-
-// click input
-numberBtn.forEach(button => {
-  button.addEventListener('click', () => appendNumber(button.textContent))
-});
-
-operatorBtn.forEach(button => {
-  button.addEventListener('click', () => operate(button.textContent))
-})
-
-
-function appendNumber(number) {
-  if (currentOperation.textContent === '0' || resetScreen) {
-    resetScreen()
-  }
-  currentOperation.textContent += number
-}
-
-// function resetScreen() {
-//
-// }
-
-function clear() {
-  currentOperation.textContent = null
-  previousOperation.textContent = ''
-}
-
-// basic calculations
-const add = (x , y) => x + y;
-const substract = (x, y) => x - y;
-const mutiply = (x, y) => x * y;
-const divide = (x, y) => x / y;
-
-const operate = (operator, x, y) => {
-  x = Number(x);
-  y = Number(y);
-
-  switch (operator) {
-    case '+':
-      return add(x, y);
-    case '-':
-      return substract(x, y);
-    case '*':
-      return mutiply(x, y);
-    case '/':
-      return divide(x, y);
-    default:
-      return null
+  if (e.key >= 0 && e.key <= 9) {
+    calculator.appendNumber(e.key);
+    calculator.updateDisplay()
   }
 }
 
 
 // set
+const calculator = new Calculator(previousOperandTextEl, currentOperandTextEl);
 
-// clearBtn.addEventListener('click', clear())
+numberBtn.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay();
+  })
+})
+
+operatorBtn.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.chooseOperation(button.innerText)
+    calculator.updateDisplay()
+  })
+})
+
+equalBtn.addEventListener('click', () => {
+  calculator.operate();
+  calculator.updateDisplay();
+})
+
+clearBtn.addEventListener('click', () => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+
+delBtn.addEventListener('click', () => {
+  calculator.delete();
+  calculator.updateDisplay();
+})
+
+window.addEventListener('keydown', handleKeyboardInput);
